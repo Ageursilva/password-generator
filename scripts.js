674383d2370a473e71be31e5e76c5a56071de056
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2023-04-11 07:47:30 -0300

    Melhora na UX e adição de botões seleção

--- scripts.js
+++ scripts.js
@@ -1,15 +1,32 @@
 let passwordLenght = 16     
 const inputEl = document.querySelector ("#password")
 
+const upperCaseCheckEl =  document.querySelector('#uppercase-check')
+const numberCheckEl =  document.querySelector('#number-check')
+const symbolCheckEl =  document.querySelector('#symbol-check')
    function  generatePassword() {
-    const chars =
-        "abcçdefghijklmnopqrstuvwxyzABCÇDEFGHIJKLMNOPQRSTUVWXYZ1234565789!#$%&()*+,-./:;<=>?@[]^_{|}"
+    let chars ="abcçdefghijklmnopqrstuvwxyz"
     
-        let password =''
-    
-        for(let i= 0; i< passwordLenght;i++){
-            const randomNumber = Math.floor(Math.random()* chars.length)
-            password += chars.substring(randomNumber,randomNumber + 1)
+    const upperCaseChars = "ABCÇDEFGHIJKLMNOPQRSTUVWXYZ" 
+    const numberChars = "1234565789"
+    const  symbolChars = "!#$%&()*+,-./:;<=>?@[]^_{|}"
+
+    if (upperCaseCheckEl.checked){
+        chars += upperCaseChars
+    }
+    if (numberCheckEl.checked){
+        chars += numberChars
+    }
+     
+    if (symbolCheckEl.checked){
+        chars += symbolChars
+    }     
+        
+    let password =''
+            
+    for(let i= 0; i< passwordLenght;i++){
+        const randomNumber = Math.floor(Math.random()* chars.length)
+        password += chars.substring(randomNumber,randomNumber + 1)
     }
     
       inputEl.value= password
@@ -24,6 +41,9 @@ passwordLenghtEl.addEventListener("input",function(){
     passwordLenght=passwordLenghtEl.value
     generatePassword()
 })
+upperCaseCheckEl.addEventListener('click',generatePassword)
+numberCheckEl.addEventListener('click',generatePassword)
+symbolCheckEl.addEventListener('click',generatePassword)
 document.querySelector("#copy-1").addEventListener('click',copy)
 document.querySelector("#copy-2").addEventListener('click',copy)
 
